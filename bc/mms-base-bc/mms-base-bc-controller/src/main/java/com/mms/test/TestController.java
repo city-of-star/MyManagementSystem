e528b2d19c93748afb3d50781df58234ca0d7bb1
li.hongyu <2722562862@qq.com>
2025-10-16 09:56:10 +0800

    重构项目目录，统一添加 mms- 前缀

---
+++ bc/mms-base-bc/mms-base-bc-controller/src/main/java/com/mms/test/TestController.java
@@ -0,0 +1,32 @@
+package com.mms.test;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mms.test.dto.TestDTO;
+import com.mms.test.entity.TestEntity;
+import com.mms.test.service.TestService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 实现功能【测试基础功能】
+ *
+ * @author li.hongyu
+ * @date 2025-10-12 19:33:28
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @GetMapping("/1")
+    public String test1() {
+        return testService.test();
+    }
+
+    @PostMapping("/getPage")
+    public Page<TestEntity> getPage(@RequestBody TestDTO dto) {
+        return testService.getPage(dto);
+    }
+}
