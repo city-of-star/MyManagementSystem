e827b7c97f75f7b7a84211d5d18c42c071f309de
li.hongyu <li.hongyu@ustcinfo.com>
2025-11-05 11:19:17 +0800

    修改了nacos内的数据库连接配置，现在统一连接服务器的数据库，现计划本地和生产两个环境的建表sql

---
+++ bc/mysql/init_mms_prod_core.sql
@@ -0,0 +1,18 @@
+-- 创建 mms_dev_core 数据库
+CREATE DATABASE `mms_prod_core` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- 使用该数据库
+USE `mms_prod_core`;
+
+-- 创建 test 表
+CREATE TABLE test
+(
+    id          bigint auto_increment comment '主键'
+        primary key,
+    title       varchar(512) not null default '' comment '测试标题',
+    content     text comment '测试内容',
+    create_time datetime     not null default CURRENT_TIMESTAMP comment '创建时间',
+    update_time datetime     not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间',
+    INDEX idx_title (title),
+    INDEX idx_create_time (create_time)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci comment '测试表-用于测试服务基础功能';
