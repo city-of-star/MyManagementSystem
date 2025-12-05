27a781f3c0ffe7ec76ac622cd9207847edbc431e
li.hongyu <li.hongyu@ustcinfo.com>
2025-12-05 11:39:37 +0800

    配置swagger文档【响应示例】

--- bc/mms-common-bc/mms-common-bc-core/src/main/java/com/mms/common/core/response/Response.java
+++ bc/mms-common-bc/mms-common-bc-core/src/main/java/com/mms/common/core/response/Response.java
@@ -1,5 +1,6 @@
 package com.mms.common.core.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.slf4j.MDC;
 
@@ -10,6 +11,7 @@ import org.slf4j.MDC;
  * @date 2025-10-28 15:51:29
  */
 @Data
+@Schema(description = "统一响应包装类")
 public class Response<T> {
 
     // 成功消息
@@ -19,9 +21,16 @@ public class Response<T> {
     // 业务失败基础码
     public static final int BUSINESS_ERROR_BASE = 1000;
 
+    @Schema(description = "响应状态码，200表示成功", example = "200")
     private Integer code;
+
+    @Schema(description = "响应消息", example = "success")
     private String message;
+
+    @Schema(description = "响应数据")
     private T data;
+
+    @Schema(description = "追踪ID，用于链路追踪", example = "04552a3a476749d98ea34f3568e3f67c")
     private String traceId = MDC.get("traceId");
 
     // 私有构造函数
